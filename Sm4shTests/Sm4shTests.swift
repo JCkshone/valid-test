26020304ed002212fd14c0cb71743af90dd26f6f
Juan Camilo <juancamilo@coink.com>
2019-02-22 14:10:03 -0500

    Initial Commit

---
+++ Sm4shTests/Sm4shTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sm4shTests.swift
+//  Sm4shTests
+//
+//  Created by JC Navarro on 2/22/19.
+//  Copyright © 2019 JC Navarro. All rights reserved.
+//
+
+import XCTest
+@testable import Sm4sh
+
+class Sm4shTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
